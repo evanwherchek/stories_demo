957725bbe4adeac5af1f4dbb62ed733b53c5939d
evanwherchek <evanhsignin@gmail.com>
2024-01-18 13:53:34 -0600

    Added read and unread segments to the ring

--- lib/home.dart
+++ lib/home.dart
@@ -12,6 +12,9 @@ class _HomeState extends State<Home> with SingleTickerProviderStateMixin {
   final String _imagePath = 'assets/mona-lisa.png';
   final String _username = 'monalisa';
   late AnimationController _controller;
+  final int _totalItems = 5;
+  late int _unreadItems;
+  late int _readItems;
 
   @override
   void initState() {
@@ -19,6 +22,9 @@ class _HomeState extends State<Home> with SingleTickerProviderStateMixin {
 
     _controller = AnimationController(
         duration: const Duration(milliseconds: 500), vsync: this);
+
+    _unreadItems = _totalItems;
+    _readItems = 0;
   }
 
   @override
@@ -32,7 +38,11 @@ class _HomeState extends State<Home> with SingleTickerProviderStateMixin {
                 _controller.forward().then((_) {
                   _controller.reverse();
 
-                  Navigator.pushNamed(context, '/view_story');
+                  Navigator.pushNamed(context, '/view_story',
+                          arguments: _totalItems)
+                      .then((value) {
+                        
+                      });
                 });
               },
               child: ScaleTransition(
@@ -42,9 +52,7 @@ class _HomeState extends State<Home> with SingleTickerProviderStateMixin {
                   width: 150.0,
                   height: 150.0,
                   child: CustomPaint(
-                    painter: RingPainter(
-                      segmentCount: 3,
-                    ),
+                    painter: RingPainter(readSegments: 4, unreadSegments: 5),
                     child: Padding(
                       padding: const EdgeInsets.all(10.0),
                       child: CircleAvatar(
@@ -81,22 +89,29 @@ class _HomeState extends State<Home> with SingleTickerProviderStateMixin {
 class RingPainter extends CustomPainter {
   final double strokeWidth = 5.0;
   final double gapSize = 25.0;
-  final Gradient gradient = const LinearGradient(
-    colors: [
-      Color(0xff8a3ab9),
-      Color(0xffe95950),
-      Color(0xffbc2a8d),
-      Color(0xffcd486b),
-      Color(0xff4c68d7)
-    ],
-    begin: Alignment.topRight,
-    end: Alignment.bottomLeft
-  );
 
-  final int segmentCount;
+  final Gradient gradientUnread = const LinearGradient(colors: [
+    Color(0xff8a3ab9),
+    Color(0xffe95950),
+    Color(0xffbc2a8d),
+    Color(0xffcd486b),
+    Color(0xff4c68d7)
+  ], begin: Alignment.topRight, end: Alignment.bottomLeft);
+
+  final Gradient gradientRead = const LinearGradient(colors: [
+    Color.fromARGB(38, 138, 58, 185),
+    Color.fromARGB(39, 233, 90, 80),
+    Color.fromARGB(56, 188, 42, 142),
+    Color.fromARGB(39, 205, 72, 107),
+    Color.fromARGB(42, 76, 104, 215)
+  ], begin: Alignment.topRight, end: Alignment.bottomLeft);
+
+  final int readSegments;
+  final int unreadSegments;
 
   RingPainter({
-    this.segmentCount = 1,
+    this.unreadSegments = 1,
+    this.readSegments = 0,
   });
 
   @override
@@ -104,16 +119,23 @@ class RingPainter extends CustomPainter {
     final Rect rect = Offset.zero & size;
 
     final paint = Paint()
-      ..shader = gradient.createShader(rect)
+      ..shader = gradientUnread.createShader(rect)
       ..strokeWidth = strokeWidth
       ..style = PaintingStyle.stroke;
 
     final radius = size.width / 2;
     final gapAngle = gapSize / radius;
 
-    for (int i = 0; i < segmentCount; i++) {
-      final startAngle = i * 2 * pi / segmentCount;
-      final endAngle = startAngle + 2 * pi / segmentCount - gapAngle;
+    int totalSegments = unreadSegments + readSegments;
+
+    for (int i = 0; i < totalSegments; i++) {
+      final startAngle = i * 2 * pi / totalSegments;
+      final endAngle = startAngle + 2 * pi / totalSegments - gapAngle;
+
+      if (i >= unreadSegments) {
+        paint.shader = gradientRead.createShader(rect);
+      }
+
       canvas.drawArc(
         Rect.fromCircle(center: size.center(Offset.zero), radius: radius),
         startAngle,
--- lib/view_story.dart
+++ lib/view_story.dart
@@ -10,29 +10,44 @@ class ViewStory extends StatefulWidget {
 
 class _ViewStoryState extends State<ViewStory> {
   final StoryController _storyController = StoryController();
+  final List<StoryItem> _storyItems = [];
+  late int _totalItems;
 
-  final List<StoryItem> _storyItems = [
-    StoryItem.text(title: 'One', backgroundColor: Colors.blue),
-    StoryItem.text(title: 'Two', backgroundColor: Colors.red),
-    StoryItem.text(title: 'Three', backgroundColor: Colors.green),
-  ];
+  void _buildStoryItems() {
+    for (int i = 0; i < _totalItems; i++) {
+      _storyItems
+          .add(StoryItem.text(title: '$i', backgroundColor: Colors.blueAccent));
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    Future.delayed(Duration.zero).then((value) {
+      _totalItems = ModalRoute.of(context)!.settings.arguments as int;
+      assert(_totalItems > 0);
+
+      _buildStoryItems();
+    });
+    
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
         child: StoryView(
-          storyItems: _storyItems,
-          controller: _storyController,
-          repeat: true,
-          onStoryShow: (s) {},
-          onComplete: () {},
-          onVerticalSwipeComplete: (direction) {
-            if (direction == Direction.down) {
-              Navigator.pop(context);
-            }
-          }
-        ),
+            storyItems: _storyItems,
+            controller: _storyController,
+            repeat: true,
+            onStoryShow: (s) {},
+            onComplete: () {},
+            onVerticalSwipeComplete: (direction) {
+              if (direction == Direction.down) {
+                Navigator.pop(context);
+              }
+            }),
       ),
     );
   }
