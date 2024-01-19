21ee5bc22647b962defb8555784d6b8ec814687f
evanwherchek <evanhsignin@gmail.com>
2024-01-19 10:16:28 -0600

    Fixed error in building stories

--- lib/view_story.dart
+++ lib/view_story.dart
@@ -11,30 +11,22 @@ class ViewStory extends StatefulWidget {
 class _ViewStoryState extends State<ViewStory> {
   final StoryController _storyController = StoryController();
   final List<StoryItem> _storyItems = [];
-  late int _totalItems;
+  late int _totalItems = 0;
 
   void _buildStoryItems() {
     for (int i = 0; i < _totalItems; i++) {
       _storyItems
-          .add(StoryItem.text(title: '$i', backgroundColor: Colors.blueAccent));
+          .add(StoryItem.text(title: '${i + 1}', backgroundColor: Colors.blueAccent));
     }
   }
 
   @override
-  void initState() {
-    super.initState();
-
-    Future.delayed(Duration.zero).then((value) {
-      _totalItems = ModalRoute.of(context)!.settings.arguments as int;
-      assert(_totalItems > 0);
-
-      _buildStoryItems();
-    });
-    
-  }
-
-  @override
   Widget build(BuildContext context) {
+    _totalItems = ModalRoute.of(context)!.settings.arguments as int;
+    assert(_totalItems > 0);
+
+    _buildStoryItems();
+
     return Scaffold(
       body: SafeArea(
         child: StoryView(
